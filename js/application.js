b7a1861f54792339ef2f7f13c8dad0fe1d44b1ee
Kevin Østerkilde <kevin@oesterkilde.dk>
2017-04-24 15:07:23 +0200

    Changed marker icon

--- js/application.js
+++ js/application.js
@@ -86,9 +86,9 @@ window.onload = function() {
 	var spot;
 	function setMarkers(map) {
 		var image = {
-      url: 'https://www.dunelondon.com/mobile/images/core/pin-concession.png',
+      url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
       // This marker is 20 pixels wide by 32 pixels high.
-      size: new google.maps.Size(20, 32),
+      size: new google.maps.Size(26, 32),
       // The origin for this image is (0, 0).
       origin: new google.maps.Point(0, 0),
       // The anchor for this image is the base of the flagpole at (0, 32).
@@ -203,6 +203,6 @@ window.onload = function() {
 	});
 	$("#hide").click(function() {
 		marker.setVisible(false);
-	})
+	});
 
 }
